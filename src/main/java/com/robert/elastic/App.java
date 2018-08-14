f380e35a25bbe59b87c23b777ba12e776d80248c
Robert Voit <robert.voit@dsainc.com>
2018-08-14 09:24:41 -0400

    "1st version of json parser to ES"

---
+++ src/main/java/com/robert/elastic/App.java
@@ -0,0 +1,86 @@
+package com.robert.elastic;
+import java.io.FileReader;
+import java.util.Iterator;
+import java.util.Map;
+import java.io.PrintWriter;
+
+import org.apache.http.HttpHost;
+import org.elasticsearch.action.index.IndexRequest;
+import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.common.xcontent.XContentType;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.*;
+
+
+/**
+ * Java high level rest api test
+ *
+ */
+public class App 
+{
+    public static void main( String[] args ) throws Exception
+    {
+        RestHighLevelClient client = new RestHighLevelClient(
+                RestClient.builder(
+                        new HttpHost("localhost", 9200, "http")));
+
+        // parsing file "with json objects"
+        Object obj = new JSONParser().parse(new FileReader("C:/Users/robert.voit/Documents/Programming/Elastic/collections/bulk json/ingest_doj_short.json"));
+
+        // typecasting obj to JSONObject
+        JSONObject jo_input = (JSONObject) obj;
+
+        //object for parsed json objects
+        JSONObject jo_output = new JSONObject();
+
+        //iterate over the json authority array
+        JSONArray auth = (JSONArray) jo_input.get("Authorities");
+        Iterator authIterator = auth.iterator();
+
+
+        //PrintWriter pw = new PrintWriter("JSONExample.json");
+
+
+        while (authIterator.hasNext())
+        {
+            Iterator<Map.Entry> itr1 = ((Map) authIterator.next()).entrySet().iterator();
+
+            //Map m = new LinkedHashMap();
+            while (itr1.hasNext()) {
+                Map.Entry pair = itr1.next();
+                if(pair.getKey().equals("AuthorityType")){
+                    continue;
+                }
+                else if(pair.getKey().equals("AuthorityTypeText")){
+                    jo_output.put("AuthorityType", pair.getValue());
+                }
+                else {
+                    jo_output.put(pair.getKey(), pair.getValue());
+                }
+            }
+
+            /*Index parsed json object to elastic search*/
+            IndexRequest request = new IndexRequest("authority_index_test", "_doc");
+            request.source(jo_output, XContentType.JSON);
+            client.index(request);
+
+            //for writing to local file
+            /*pw.write(jo_output.toJSONString());
+            if(authIterator.hasNext()) {
+                pw.write(", \n");
+            }
+            pw.flush();*/
+
+        }
+
+        //pw.close();
+
+        //close client connection
+        client.close();
+
+
+
+    }
+}
